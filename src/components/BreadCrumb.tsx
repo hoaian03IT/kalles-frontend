55f8ec86d6f3354f5ba0a2576445d37ad6f84827
hoaian03 <dangtranhoaian8603@gmail.com>
2024-03-26 23:40:05 +0700

    breadcrumbs

---
+++ src/components/BreadCrumb.tsx
@@ -0,0 +1,30 @@
+import classNames from "classnames/bind";
+import { Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import styles from "~/styles/BreadCrumb.module.scss";
+import { MdNavigateNext } from "react-icons/md";
+
+const cx = classNames.bind(styles);
+
+type Props = {
+    links: Array<{ label: string; value: string; isCurrent: boolean }>;
+};
+
+export const BreadCrumb = ({ links }: Props) => {
+    return (
+        <div className={cx("wrapper")}>
+            <Container>
+                <div className={cx("links", "d-flex")}>
+                    {links.map((link, index) => (
+                        <div key={index} className={cx("link-wrap")}>
+                            <Link className={cx("link", link.isCurrent ? "current" : "")} to={link.value}>
+                                {link.label}
+                            </Link>
+                            {index !== links.length - 1 && <MdNavigateNext className="mx-1" />}
+                        </div>
+                    ))}
+                </div>
+            </Container>
+        </div>
+    );
+};
